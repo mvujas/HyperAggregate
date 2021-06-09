70473fee204e586ea326591843e9c833d02bc1bc
mvujas <18309829+mvujas@users.noreply.github.com>
2021-06-09 23:35:47 +0200

    Documentation updates

--- middleware_test_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-gnome-terminal -t "Server" -x bash -c "python protocol-prototype/run_server.py --port 9998 --target-size 3 --group-size 3 --num-actors 2; read line"
-gnome-terminal -t "Middleware server 1" -x bash -c "python protocol-prototype/middleware_server.py --port 9997 --aggregation-client-port 9001 --aggregation-server-address 127.0.0.1:9998; read line"
-gnome-terminal -t "Middleware server 2" -x bash -c "python protocol-prototype/middleware_server.py --port 9996 --aggregation-client-port 9002 --aggregation-server-address 127.0.0.1:9998; read line"
-gnome-terminal -t "Middleware server 3" -x bash -c "python protocol-prototype/middleware_server.py --port 9995 --aggregation-client-port 9003 --aggregation-server-address 127.0.0.1:9998; read line"
-
-gnome-terminal -t "Middleware client 1" -x bash -c "node middleware/client/client_test.js 9997; read line"
-gnome-terminal -t "Middleware client 2" -x bash -c "node middleware/client/client_test.js 9996; read line"
-gnome-terminal -t "Middleware client 3" -x bash -c "node middleware/client/client_test.js 9995; read line"
--- protocol_prototype/docs/createdoc.sh
+++ protocol_prototype/docs/createdoc.sh
@@ -1,4 +1,4 @@
-export PYTHONPATH="$PYTHONPATH:../env/lib/python3.8/site-packages"
+export PYTHONPATH="$PYTHONPATH:../env/lib/python3.8/site-packages" # Change to the location where you have your packages installed
 sphinx-apidoc --force -o . ../src
 make clean
 make html
